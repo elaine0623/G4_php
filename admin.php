6ee1ccbc57f11fb1ca21294119216a8cc729d3d6
Bruce1219 <891219chiang@gmail.com>
2024-06-30 09:30:51 +0800

    admin

---
+++ admin.php
@@ -0,0 +1,39 @@
+<?php
+try {
+    require_once("./connect_cid101g4.php");
+    $returnData = [
+        'code' => 200,
+        'msg' => '',
+        'data' => []
+    ];
+    //抓前端傳來的資料
+    $data = json_decode(file_get_contents('php://input'), true);
+    $checkAccount = "SELECT * FROM admin WHERE am_account = :am_account AND am_password = :am_password";
+    $stmt = $pdo->prepare($checkAccount);
+    $stmt->bindValue(':am_account', $data['acc']);
+    $stmt->bindValue(':am_password', $data['psw']);
+    $stmt->execute();
+    if ($stmt->rowCount() > 0) {
+        $userData = $stmt->fetch(PDO::FETCH_ASSOC);
+        //判斷是否停權
+        $permission = $userData['am_status'];
+        if ($permission == 0) {
+            $returnData['code'] = 10004;
+            $returnData['msg'] = "此帳號已停權";
+        }else{
+            session_start();
+            $_SESSION = $userData;
+            $returnData['code'] = 200;
+            $returnData['data'] = $_SESSION;
+        }
+    }else{
+        $returnData['code'] = 10002;
+        $returnData['msg'] = "帳號或密碼錯誤";
+    }
+} catch (Exception $e) { // 更廣泛地捕獲異常
+    $returnData['code'] = 10003;
+    $returnData['msg'] = $e->getMessage();
+}
+
+echo json_encode($returnData);
+?>
