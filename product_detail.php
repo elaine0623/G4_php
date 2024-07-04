4a620707444d61215caf4be1d24e9f7811452975
elaine0623 <0148004@gmail.com>
2024-07-04 09:07:14 +0800

    商品列表/商品內頁/會員加入購物車及收藏

--- product_detail.php
+++ product_detail.php
@@ -9,20 +9,17 @@ try {
     ];
     //抓前端傳來的資料
     $data = json_decode(file_get_contents('php://input'), true);
+    $userNo = $data['userNo'];
+    $prodNo = $data['p_no'];
     //執行分頁查詢sql
     // $sql = "SELECT * FROM product WHERE p_status = 1  ORDER BY p_no desc limit $limit OFFSET $size";
     $sql = "SELECT *
 FROM product p
 JOIN farm f ON p.f_no = f.f_no 
 JOIN product_category c ON p.pc_no = c.pc_no
-WHERE p.p_status = 1 
-ORDER BY p.p_no DESC ";
+WHERE p.p_no = $prodNo";
     $product = $pdo->prepare($sql);
     $product->execute();
-    //查詢所有有效商品筆數
-    $sql2 = "SELECT * FROM product WHERE p_status = 1";
-    $stmt = $pdo->prepare($sql2);
-    $stmt->execute();
     //抓取資料庫商品資料
     $productData = $product->fetchAll(PDO::FETCH_ASSOC);
     foreach($productData as $key => $prod){
@@ -31,14 +28,26 @@ ORDER BY p.p_no DESC ";
         $product_img = $pdo->prepare($sql3);
         $product_img->execute();
         $proDetails = $product_img->fetchAll(PDO::FETCH_ASSOC);
+        $productData[$key]['isaddCart'] = false;
         foreach($proDetails as $proDetail){
             $productData[$key]['p_img'][] = $proDetail['pi_img'];//第0~N張照片放進[]裡(類似array.push)
         }
+            //fetch會員資料庫購物車及收藏商品內容
+            $sql4 = "SELECT * FROM member_favorite WHERE m_no  = '$userNo' AND p_no = $prodNo";
+            $favoriteCart = $pdo->prepare($sql4);
+            $favoriteCart->execute();
+            $favoriteCarts = $favoriteCart->fetchAll(PDO::FETCH_ASSOC);
+            foreach($favoriteCarts as $fav){
+                if($fav['fav'] == 1 && $fav['p_no'] == $prodNo){
+                    $productData[$key]['isImage1'] = true;
+                }
+                if($fav['cart'] == 1 && $fav['p_no'] == $prodNo){
+                    $productData[$key]['isaddCart'] = true;
+                }
+            }
     }
+    
     $returnData['data']['list'] = $productData;
- 
-    $totalCount = $stmt->rowCount();
-
 } catch (Exception $e) { // 更廣泛地捕獲異常
     $returnData['code'] = 10003;
     $returnData['msg'] = $e->getMessage();
