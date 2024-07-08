14d826626a362b32a98a1257d3b8a8bf17da2329
elaine0623 <0148004@gmail.com>
2024-07-08 13:54:56 +0800

    在上傳成功後break

--- addEventImage.php
+++ addEventImage.php
@@ -32,8 +32,10 @@ try {
             $fileName = $_FILES['a_img']['name'];
             $from = $_FILES['a_img']['tmp_name'];
             $to = "$dir/$fileName";
+            // exit($to."===");
             copy($from,$to);
             echo "上傳成功!!";
+            break;
             case UPLOAD_ERR_INI_SIZE:
                 echo "上傳檔案太大, 不得超過", ini_get("upload_max_filesize"), "<br>"; 
                 break;
