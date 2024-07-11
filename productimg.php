60b378a7c6136f60e1c59248a689c75f598ac61d
Aiden911 <forest1991911@mail.com>
2024-07-11 12:12:23 +0800

    更改路徑

--- productimg.php
+++ productimg.php
@@ -20,8 +20,8 @@ try {
     }
 
     // 設置文件上傳目錄
-    $uploadDir = 'D:\g4_0607\G4_frontend\src\assets\image';
-    // $uploadDir =  '../images/assets';
+    //$uploadDir = 'D:\g4_0607\G4_frontend\src\assets\image';
+    $uploadDir =  '../images/assets';
 
     // 檢查目錄是否存在,不存在則創建
     if (!file_exists($uploadDir)) {
